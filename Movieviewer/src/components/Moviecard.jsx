3511d1e7ad592e3292e4e193698b6c996ba3efc1
GRagavi <sharmi9318@gmail.com>
2025-11-14 22:25:06 +0530

    14Nov

--- Movieviewer/src/components/Moviecard.jsx
+++ Movieviewer/src/components/Moviecard.jsx
@@ -6,7 +6,7 @@ function Moviecard({movie}){
     }
 
     return <div className="movie-card">
-            <img src={movie.url} alt={movie.title}/>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
                 <button className="favourite-btn" onClick={handleFavouriteClick}>
                     ❤️
@@ -14,7 +14,7 @@ function Moviecard({movie}){
             </div>
             <div className="movie-info">
                 <h3>{movie.title}</h3>
-                <p><strong>Release Year:</strong>{movie.releaseyear}</p>
+                <p><strong>Release Year:</strong>{movie.release_date?.split("-")[0]}</p>
                 <p><strong>Rating:</strong> {movie.rating}/10</p>
             </div>
         </div>
